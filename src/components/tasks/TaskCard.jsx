6e467a14e31ee6fbfdd3d65ac80e6017494cea62
Tanjil Ahamed <tanjil113355@gmail.com>
2024-02-27 21:25:28 +0600

    naw all the task title colord by their priority

--- src/components/tasks/TaskCard.jsx
+++ src/components/tasks/TaskCard.jsx
@@ -17,15 +17,15 @@ const TaskCard = ({ task }) => {
 
   return (
     <div className="bg-secondary/10 rounded-md p-5">
-      {/* <h1
+      <h1
         className={`text-lg font-semibold mb-3  ${
-          task.priority === "high" ? "text-red-500" : ""
+          task.priority === "High" ? "text-red-500" : ""
         } ${task.priority === "medium" ? "text-yellow-500" : ""} ${
           task.priority === "low" ? "text-green-500" : ""
         }`}
       >
         {task?.title}
-      </h1> */}
+      </h1>
       <p className="mb-3">{task?.description}</p>
       <p className="text-sm">Assigned to - {task?.assignedTo}</p>
       <div className="flex justify-between mt-3">
