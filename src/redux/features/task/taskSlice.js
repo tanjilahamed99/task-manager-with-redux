315829a14addc94d0e9227e78ef8f773a6baff32
Tanjil Ahamed <tanjil113355@gmail.com>
2024-02-27 12:18:48 +0600

    now all the data have special in and status pending

--- src/redux/features/task/taskSlice.js
+++ src/redux/features/task/taskSlice.js
@@ -9,11 +9,20 @@ const taskSlice = createSlice({
   initialState,
   reducers: {
     addTask: (state, { payload }) => {
-      state.task.push(payload);
+      if (state.task.length === 0) {
+        state.task.push({ id: 1, status: "pending", ...payload });
+      } else {
+        const lastElement = state.task.at(-1);
+        state.task.push({
+          id: lastElement.id + 1,
+          status: "pending",
+          ...payload,
+        });
+      }
     },
   },
 });
 
-export const {addTask} =  taskSlice.actions
+export const { addTask } = taskSlice.actions;
 
 export default taskSlice.reducer;
